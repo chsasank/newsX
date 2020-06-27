d0a5b3c16a32df7fb147fd39106c1e9f209b2286
Sasank Chilamkurthy <sasankchilamkurthy@gmail.com>
2020-06-27 11:16:53 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, {useEffect} from 'react';
-import './App.css';
-import NewsSection from './components/newsDeck'
-import Header from './components/header'
+import React, { useEffect } from "react";
+import NewsSection from "./components/newsDeck";
+import Header from "./components/header";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link,
   useParams,
   useLocation,
 } from "react-router-dom";
-
+import "./App.css";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -22,37 +20,37 @@ function ScrollToTop() {
   return null;
 }
 
-
 function Home() {
-  return <>
-    <NewsSection tag="latest"/>
-    <NewsSection tag="markets"/>
-    <NewsSection tag="economy"/>
-  </>
+  return (
+    <>
+      <NewsSection tag="latest" />
+      <NewsSection tag="markets" />
+      <NewsSection tag="economy" />
+    </>
+  );
 }
 
 function TagPage() {
   let { tag } = useParams();
-  if (tag == null){
-    return <h2>Please select a tag</h2>
+  if (tag == null) {
+    return <h2>Please select a tag</h2>;
   }
-  return <NewsSection tag={tag} numArticles={20}/>
+  return <NewsSection tag={tag} numArticles={20} />;
 }
-
 
 function App() {
   return (
     <Router>
       <ScrollToTop />
       <div className="container">
-        <Header/>
+        <Header />
 
         <Switch>
           <Route exact path="/">
-            <Home/>
+            <Home />
           </Route>
           <Route path="/tag/:tag">
-            <TagPage/>
+            <TagPage />
           </Route>
         </Switch>
       </div>
--- src/components/newsCard.js
+++ src/components/newsCard.js
@@ -1,46 +1,44 @@
-import React from 'react';
-import moment from 'moment'
-import {Link} from 'react-router-dom'
+import React from "react";
+import moment from "moment";
+import { Link } from "react-router-dom";
 
-
-function RelatedNews(props){
-  if (props.relatedArticles == null){
-    return <></>
+function RelatedNews(props) {
+  if (props.relatedArticles == null) {
+    return <></>;
   }
-  return(
+  return (
     <ul className="news-related list-group list-group-flush">
-      {
-        props.relatedArticles.map(article => (
-          <li className="list-group-item">
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
-              {article.title}
-            </a>
-            <NewsMetadata article={article}/>
-          </li>
-        ))
-      }
+      {props.relatedArticles.map((article) => (
+        <li className="list-group-item">
+          <a href={article.url} target="_blank" rel="noopener noreferrer">
+            {article.title}
+          </a>
+          <NewsMetadata article={article} />
+        </li>
+      ))}
     </ul>
-  )
+  );
 }
 
-
-function NewsMetadata(props){
-  let website = props.article.website
-  let date = moment(props.article.date, 'X').utc()
+function NewsMetadata(props) {
+  let website = props.article.website;
+  let date = moment(props.article.date, "X").utc();
   let websiteLookup = {
-    'moneycontrol': 'Moneycontrol',
-    'thehindubusinessline': 'Business Line'
-  }
-  return <small>
-    <span className="news-time" title={date.format("D MMMM Y h:mm A")}>
-      {date.fromNow()}
-    </span> - <span className="news-source">{websiteLookup[website]}</span>
-  </small>
+    moneycontrol: "Moneycontrol",
+    thehindubusinessline: "Business Line",
+  };
+  return (
+    <small>
+      <span className="news-time" title={date.format("D MMMM Y h:mm A")}>
+        {date.fromNow()}
+      </span>{" "}
+      - <span className="news-source">{websiteLookup[website]}</span>
+    </small>
+  );
 }
 
-
-function NewsCard(props){
-  let article = props.article
+function NewsCard(props) {
+  let article = props.article;
 
   return (
     <div className="news-card card">
@@ -50,25 +48,25 @@ function NewsCard(props){
             {article.title}
           </a>
         </h5>
-        <div className='news-body'>
+        <div className="news-body">
           <p className="news-description">{article.description}</p>
-          <p className="news-meta"><NewsMetadata article={article}/></p>
-          <RelatedNews relatedArticles={article.related_articles}/>
+          <p className="news-meta">
+            <NewsMetadata article={article} />
+          </p>
+          <RelatedNews relatedArticles={article.related_articles} />
         </div>
         <div className="news-footer">
           <div className="news-tags">
-            {
-              article.tags.map(tag => (
-                <span className="news-hash-tag" key={tag}>
-                  <Link to={`/tag/${tag}`}>#{tag}</Link>
-                </span>
-              ))
-            }
+            {article.tags.map((tag) => (
+              <span className="news-hash-tag" key={tag}>
+                <Link to={`/tag/${tag}`}>#{tag}</Link>
+              </span>
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default NewsCard;
